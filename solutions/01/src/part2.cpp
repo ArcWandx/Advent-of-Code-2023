d24af7d98bc0e295b0e4006bea51ac31927dccef
Robert Zhu <arcwandx86@gmail.com>
2024-01-03 13:35:59 -0500

    Make part 2 code more logical

--- solutions/01/src/part2.cpp
+++ solutions/01/src/part2.cpp
@@ -17,22 +17,22 @@ int digit_length(int n) {
 	}
 }
 
-int to_digit(string line, int i) {
+char to_digit(string line, int i) {
 	// If substring starting at i matches "one", "two", etc., return the corresponding digit
 	// Otherwise, return -1
 	for (int j = 1; j <= 9; j++) {
 		string s = line.substr(i, digit_length(j));
-		if (s == "one") return 1;
-		if (s == "two") return 2;
-		if (s == "three") return 3;
-		if (s == "four") return 4;
-		if (s == "five") return 5;
-		if (s == "six") return 6;
-		if (s == "seven") return 7;
-		if (s == "eight") return 8;
-		if (s == "nine") return 9;
+		if (s == "one") return '1';
+		if (s == "two") return '2';
+		if (s == "three") return '3';
+		if (s == "four") return '4';
+		if (s == "five") return '5';
+		if (s == "six") return '6';
+		if (s == "seven") return '7';
+		if (s == "eight") return '8';
+		if (s == "nine") return '9';
 	}
-	return -1;
+	return '\0';
 }
 
 int main() {
@@ -41,15 +41,12 @@ int main() {
 	while (getline(cin, line)) {
 		int first = -1, last = -1;
 		for (size_t i = 0; i < line.size(); i++) {
-			char c = line[i];
-			int d = to_digit(line, i);
-			if (isdigit(c)) {
-				if (first == -1) first = c - '0';
-				last = c - '0';
-			} else if (d != -1) {
-				if (first == -1) first = d;
-				last = d;
-			}
+			char c = to_digit(line, i);
+			if (c == 0) c = line[i];
+			if (!isdigit(c)) { continue; }
+
+			if (first == -1) first = c - '0';
+			last = c - '0';
 		}
 		int n = 10 * first + last;
 		sum += n;
